e576ac98e1fc7737fc4bbc742b3208bda4cb0ed7
Guilherme Tupinamba <guilhermetupinamba@gmail.com>
2022-04-08 21:28:33 -0300

    Loader, error e handle dados

--- frontend/src/components/ContactForm/index.js
+++ frontend/src/components/ContactForm/index.js
@@ -1,29 +1,44 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import FormGroup from '../FormGroup';
 import { ButtonContainer, Form } from './styles';
+import FormGroup from '../FormGroup';
 import Button from '../Button';
 import Input from '../Input';
 import Select from '../Select';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleName = (e) => setName(e.target.value);
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handleTelephone = (e) => setTelephone(e.target.value);
+  const handleCategory = (e) => setCategory(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <Form>
-      <FormGroup>
-        <Input type="text" placeholder="Nome" />
+    <Form onSubmit={handleSubmit}>
+      <FormGroup error="Nome é obrigatório">
+        <Input type="text" placeholder="Nome" error value={name} onChange={handleName} />
+      </FormGroup>
+
+      <FormGroup error="O formato do e-mail é inválido">
+        <Input type="email" placeholder="E-mail" error value={email} onChange={handleEmail} />
       </FormGroup>
 
       <FormGroup>
-        <Input type="email" placeholder="Email" />
+        <Input type="text" placeholder="Telefone" value={telephone} onChange={handleTelephone} />
       </FormGroup>
 
       <FormGroup>
-        <Input type="text" placeholder="Telefone" />
-      </FormGroup>
-
-      <FormGroup>
-        <Select>
+        <Select value={category} onChange={handleCategory}>
           <option value="" disabled selected hidden>
-            Selecione a categoria
+            Categoria
           </option>
           <option value="instagram">Instagram</option>
           <option value="gmail">Gmail</option>
